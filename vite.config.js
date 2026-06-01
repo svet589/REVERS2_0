ae21b858e478de43eca8ea60641c7af2161b8c2a
svet589 <d99428850@gmail.com>
2026-06-01 03:19:57 +0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
+  build: {
+    target: 'es2015',
+    outDir: 'src',
+    emptyOutDir: false,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/app.js'),
+      output: {
+        entryFileNames: 'bundle.js',
+        format: 'iife',
+        inlineDynamicImports: true
+      }
+    },
+    commonjsOptions: {
+      include: [/libsodium-wrappers/, /node_modules/]
+    }
+  },
+  optimizeDeps: {
+    include: ['libsodium-wrappers']
+  }
+});
