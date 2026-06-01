1ee1688aa3ac7f46cad6ebc46e4f4ff1a87981a5
svet589 <d99428850@gmail.com>
2026-06-02 00:19:41 +0300

    Create stickerHandlers.js

---
+++ src/handlers/stickerHandlers.js
@@ -0,0 +1,58 @@
+// src/handlers/stickerHandlers.js — обработка стикеров
+import stickerManager from '../core/sticker-manager.js';
+
+export function initStickerHandlers(eventBus) {
+  // Получить все паки
+  eventBus.on('getStickerPacks', () => {
+    const packs = stickerManager.getPacks();
+    eventBus.emit('stickerPacks', packs);
+  });
+
+  // Получить стикеры пака
+  eventBus.on('getStickers', ({ packId }) => {
+    const stickers = stickerManager.getStickers(packId);
+    eventBus.emit('stickersLoaded', { packId, stickers });
+  });
+
+  // Создать стикерпак
+  eventBus.on('createStickerPack', ({ name }) => {
+    const packId = stickerManager.createPack(name);
+    eventBus.emit('stickerPackCreated', { packId, name });
+  });
+
+  // Добавить стикер в пак
+  eventBus.on('addSticker', ({ packId, data, emoji }) => {
+    const success = stickerManager.addSticker(packId, data, emoji);
+    if (success) {
+      eventBus.emit('stickerAdded', { packId, data, emoji });
+    }
+  });
+
+  // Добавить в недавние
+  eventBus.on('addToRecent', ({ stickerData }) => {
+    stickerManager.addToRecent(stickerData);
+    eventBus.emit('recentUpdated');
+  });
+
+  // Отправить стикер
+  eventBus.on('sendSticker', ({ sticker }) => {
+    eventBus.emit('stickerSent', { sticker });
+  });
+
+  // Удалить стикерпак
+  eventBus.on('deleteStickerPack', ({ packId }) => {
+    stickerManager.deletePack(packId);
+    eventBus.emit('stickerPackDeleted', { packId });
+  });
+
+  // Чтение файла стикера
+  eventBus.on('readStickerFile', ({ file }) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      eventBus.emit('stickerFileRead', { data: e.target.result, file });
+    };
+    reader.readAsDataURL(file);
+  });
+
+  console.log('😊 Sticker handlers инициализированы');
+}
