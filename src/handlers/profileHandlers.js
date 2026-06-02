0f209635161e6dd0865babda5e891da9acbafe41
svet589 <d99428850@gmail.com>
2026-06-02 19:39:11 +0300

    Update profileHandlers.js

--- src/handlers/profileHandlers.js
+++ src/handlers/profileHandlers.js
@@ -1,4 +1,23 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This file is part of REVERS Messenger.
+ *
+ * REVERS Messenger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REVERS Messenger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with REVERS Messenger. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2025 svet589 <https://github.com/svet589>
+ */
 // src/handlers/profileHandlers.js — обработка профиля
 import identity from '../core/identity.js';
 
