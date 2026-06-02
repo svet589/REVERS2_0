fd1bc0dbd4ee38cea538ce27a558edb73feb286f
svet589 <d99428850@gmail.com>
2026-06-02 15:43:21 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 // src/app.js — точка входа REVERS Messenger v3.2
-// Лицензия: GNU GPL v3
-// Разработчик: https://github.com/svet589
+/*
+ * Copyright (C) 2025 svet589 <https://github.com/svet589>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import { EventBus } from './handlers/eventBus.js';
 import { initMessageHandlers } from './handlers/messageHandlers.js';
